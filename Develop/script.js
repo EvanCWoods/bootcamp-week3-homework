20c31cf6e39b324d5908a71b9914828a636f6ccf
Evan Woods <you@example.com>
2021-12-12 09:53:17 +1100

    Finished logic for password generation

--- Develop/script.js
+++ Develop/script.js
@@ -1,30 +1,90 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var charactersAlphabet = [
+  "a", "b", "c", "d", "e", "f", "g", "h", "i",
+  "j", "k", "l", "m", "n", "o", "p", "q", "r", 
+  "s", "t", "u", "v", "w", "x", "y", "z"
+];
+var charactersNumerical = [
+  "1", "2", "3", "4", "5", "6", "7", "8", "9",
+]
+var charactersSpecial = [
+  "!", "@", "#", "$", "%", "^", "&", "*", 
+  "(", ")", "'", "+", ",", "-", ".", "/", ":", 
+  ";", "<", ">", "=", "?", "[", "]", "_", "`", 
+  "|","{", "}", "~"
+]
+
+password = [];
 
 // Show password prompts
 function passwordPrompts() {
    // prompt for password length (8-128 characters)
    var passwordLength = prompt("What is the length of the password");
    // prompt for uppercase, lowercase, numeric, special characters
-   var passwordfCharacters = prompt("Would you like to include uppercase, lowercase, numeric or special characters?");
+   var includeUppercase = prompt("Would you like to include uppercase characters?").toUpperCase();
+   var includeLowercase = prompt("Would you like to include lowercase characters?").toUpperCase();
+   var includeNumerical = prompt("Would you like to include numerical characters?").toUpperCase();
+   var includeSpecial = prompt("Would you like to include special characters?").toUpperCase();
 
-   return [passwordLength, passwordfCharacters];
+   return [
+    passwordLength, 
+    includeUppercase, 
+    includeLowercase, 
+    includeNumerical, 
+    includeSpecial
+  ];
 }
 
 // Generate a password
 function generatePassword() {
+  // Create a new array to join the characters to
+  var characterSelectors = charactersAlphabet;
+
   // set local variables with values originating from the passwordPrompts function
   promptValues = passwordPrompts();
-  console.log(promptValues);
   passwordLength = promptValues[0];
-  passwordCharacters = promptValues[1];
-  console.log(passwordLength);
-  console.log(passwordCharacters);
+  includeUppercase = promptValues[1];
+  includeLowercase = promptValues[2];
+  includeNumerical = promptValues[3];
+  includeSpecial = promptValues[4];
+  
+  // Check for upper case
+  if (includeUppercase === "Y" && includeLowercase !== "Y") {   // if only uppercase is selected, change the entire array to uppercase
+    for (var i=0; i<charactersAlphabet.length; i++) {
+      charactersAlphabet[i] = charactersAlphabet[i].toUpperCase()
+    }
+  } else if (includeUppercase === "Y" && includeLowercase === "Y") {    // Select random index in the alphabet array to make uppercase
+    for (var i=0; i<charactersAlphabet.length / 2; i++) {
+      charactersAlphabet[
+        Math.floor(Math.random() * charactersAlphabet.length)
+      ] = charactersAlphabet[
+        Math.floor(Math.random() * charactersAlphabet.length)
+      ].toUpperCase();
+    }
+  }
+  // Check that the password inludes numerical
+  if (includeNumerical === "Y") {
+    characterSelectors = characterSelectors.concat(charactersNumerical);
+    console.log(characterSelectors);
+  }
+  // Check that the password inludes special
+  if (includeSpecial === "Y") {
+    characterSelectors = characterSelectors.concat(charactersSpecial);
+    console.log(characterSelectors);
+  }
+
 
   // Check that the password is between 8 and 128 characters
   if (passwordLength < 8 || passwordLength > 128) {
-    console.log("Password is too long, try again.")
-  }
+    prompt("Password is not between 8 and 128 characters, try again.");
+  } else {
+    for (var i=0; i<passwordLength;  i++) {   // Create a password with of the specified length
+      password.push(characterSelectors[Math.round(Math.random() * characterSelectors.length)]);   // populate an array with the characters that are randomly selected from the give narray fo choices (charactersAlphabet)
+      console.log();
+    }
+  } 
+  return password.join("");
 }
 
 // Write password to the #password input
