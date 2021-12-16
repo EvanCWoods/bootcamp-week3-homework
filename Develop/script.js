f35873af81649e00a7a52283c9d81bb18a92947a
Evan Woods <you@example.com>
2021-12-16 12:34:25 +1100

    Commented code for readability. Changed the core logic to randomly assign one quarter of the selected characters to the password.

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// Array of alphabetical cahracters
 var charactersAlphabet = [
   "a", "b", "c", "d", "e", "f", "g", "h", "i",
   "j", "k", "l", "m", "n", "o", "p", "q", "r", 
   "s", "t", "u", "v", "w", "x", "y", "z"
 ];
+
+// Array of numerical characters
 var charactersNumerical = [
   "1", "2", "3", "4", "5", "6", "7", "8", "9"
-]
+];
+
+// Array of special characters
 var charactersSpecial = [
   "!", "@", "#", "$", "%", "^", "&", "*", 
   "(", ")", "'", "+", ",", "-", ".", "/", ":", 
   ";", "<", ">", "=", "?", "[", "]", "_", "`", 
   "|","{", "}", "~"
-]
-
-password = [];
+];
 
 // Show password prompts
 function passwordPrompts() {
@@ -27,7 +31,8 @@ function passwordPrompts() {
    var includeNumerical = prompt("Would you like to include numerical characters?").toUpperCase();
    var includeSpecial = prompt("Would you like to include special characters?").toUpperCase();
 
-   return [
+  // Return an array of the values given by the user in the prompts
+   return [   
     passwordLength, 
     includeUppercase, 
     includeLowercase, 
@@ -38,6 +43,9 @@ function passwordPrompts() {
 
 // Generate a password
 function generatePassword() {
+  //  create a password array to be populated with characaters
+  password = [];
+
    // Create a new array to join the characters to
    var characterSelectors = charactersAlphabet;
 
@@ -52,46 +60,44 @@ function generatePassword() {
 
    // Check that the password is between 8 and 128 characters
    if (passwordLength < 8 || passwordLength > 128) {
-    prompt("Password is not between 8 and 128 characters, try again.");
+    prompt("Password is not between 8 and 128 characters, try again.");   // handle error for password length
   } else {
     for (var i=0; i<passwordLength;  i++) {   // Create a password with of the specified length
       password.push(characterSelectors[Math.round(Math.random() * characterSelectors.length)]);   // populate an array with the characters that are randomly selected from the give narray fo choices (charactersAlphabet)
-      console.log(password);
     }
   }
    
    // Check for upper case
   if (includeUppercase === "Y" && includeLowercase !== "Y") {
-    for (var i=0; i<password.length; i++) {
-      password[i] = password[i].toUpperCase()
+    for (var i=0; i<password.length; i++) {   // Loop through the entire password array
+      password[i] = password[i].toUpperCase()   // at every index in the array, change the character to uppercase
     }
   }
   //  Check for both upper and lower case
    if (includeUppercase === "Y" && includeLowercase === "Y") {   // if only uppercase is selected, change the entire array to uppercase
-    for (var i=0; i<Math.floor(password.length / 3); i++) {
-      upperCaseReplaceIndex = Math.floor(Math.random() * password.length);
-      password[upperCaseReplaceIndex] = password[upperCaseReplaceIndex].toUpperCase();
-      console.log(upperCaseReplaceIndex);
+    for (var i=0; i<Math.floor(password.length / 4); i++) {     // Loop through one quarter of the length of the password array
+      upperCaseReplaceIndex = Math.floor(Math.random() * password.length);    // assign an index from the password length
+      password[upperCaseReplaceIndex] = password[upperCaseReplaceIndex].toUpperCase();    // replace the index with an uppercase character
     }
   } 
    // Check that the password inludes numerical
    if (includeNumerical === "Y") {
 
-    for (var i=0; i<Math.floor(password.length / 3); i++) {
-      numericalReplaceIndex = Math.floor(Math.random() * charactersNumerical.length);
-      PasswordReplaceIndex = Math.floor(Math.random() * password.length);
-      password[PasswordReplaceIndex] = charactersNumerical[numericalReplaceIndex];
+    for (var i=0; i<Math.floor(password.length / 4); i++) {   // Loop through one quarter of the length of the password array
+      numericalReplaceIndex = Math.floor(Math.random() * charactersNumerical.length);   // Assign an index from the numerical characters array
+      PasswordReplaceIndex = Math.floor(Math.random() * password.length);   // assign an index in the password to replace with a numerical character
+      password[PasswordReplaceIndex] = charactersNumerical[numericalReplaceIndex];    // replace the character with a numerical character 
     }
    }
    // Check that the password inludes special
    if (includeSpecial === "Y") {
-    for (var i=0; i< password.length / 3; i++) {
-      specialReplaceIndex = Math.floor(Math.random() * charactersSpecial.length);
-      PasswordReplaceIndex = Math.floor(Math.random() * password.length);
-      password[PasswordReplaceIndex] = charactersSpecial[specialReplaceIndex];
+    for (var i=0; i< password.length / 4; i++) {    // Loop through one quarter of the length of the password array
+      specialReplaceIndex = Math.floor(Math.random() * charactersSpecial.length);   // assign an index from the special characters array
+      PasswordReplaceIndex = Math.floor(Math.random() * password.length);   // assign an index from the password array
+      password[PasswordReplaceIndex] = charactersSpecial[specialReplaceIndex];    // replace the password index with the special character at that index 
     }
    }
-  return password.join("");
+  return password.join("");   // Make the password a string instead of an array of single character strings
 }
 
 // Write password to the #password input
